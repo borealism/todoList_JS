03a62c85662d86d882926102fba86dbc7c2f78b8
borealism <tuncalburak@gmail.com>
2022-06-07 16:34:06 +0300

    js file edited - DOM files created

---
+++ js/todo.js
@@ -0,0 +1,7 @@
+let divListDOM = document.querySelector('#divList');
+divListDOM.addEventListener('submit', listHandler);
+
+function listHandler(event) {
+    event.preventDefault();
+    const LIST_ITEM = document.querySelector('#list');
+}
